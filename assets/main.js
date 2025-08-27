f8ce5800867739d4074385aa2cba4387bdd38516
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-08-27 06:53:50 +0330

    part156

--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,10 @@ function loadUsers() {
             <td>${user.email}</td>
             <td>${user.address.city} , ${user.address.street}</td>
 
-            <td><button>حذف</button></td>
+            <td>
+            <button onclick ="deleteUser(${user.id})">حذف</button>
+            <button>ویرایش</button>
+            </td>            
             </tr>`;
             console.log(user);            
         });
@@ -26,10 +29,11 @@ function loadUsers() {
 function addUser() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
 
 fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
-  body: JSON.stringify({name: name , email: email}),
+  body: JSON.stringify({name: name , email: email , phone: phone}),
   headers: {'Content-type': 'application/json; charset=UTF-8'}
   //headers:  یک اطلاعات به سرور می دهیم که ما داده هایمان را در چه قالبی می خواهیم ارسال بکنیم و این اطلاعات رو از بک اند دولوپر می گیریم
 })
@@ -41,13 +45,29 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
     
      //این قسمت صرفا برای نمایش اطلاعات اضافه شده است، چون دیتاهای جیسون پلیس هولدر تغییر ناپذیر است و می خواهیم تغییرات را با این وجود باز هم ببینیم
     tbody.innerHTML += `<tr>
-    <td> </td>
+    <td>${user.id}</td>
     <td>${user.name}</td>
-    <td> </td>
+    <td>${user.phone}</td>
     <td>${user.email}</td>
     <td> </td>
-    <td><button>حذف</button></td>
+    
+    <td>
+    <button onclick = "deleteUser(${user.id})">حذف</button>
+    <button>ویرایش</button>
+    </td>
+
     </tr>`;
 });
 }
 
+// حذف کاربر
+function deleteUser(id) {
+    fetch('https://jsonplaceholder.typicode.com/users/${id}', {
+      method: 'DELETE',
+    })
+    //تفاوت با دو متد بالا این است که خروجی را در پارامتر ریسپانس ذخیره نمی کنیم. چون صرفا قراره یک آی دی رو حذف کنیم
+    .then(()=>{
+        alert(`کاربر با شناسه ${id} حذف شد (فقط در ظاهر، چون API تستی است)`);
+        loadUsers(); //بازخوانی لیست
+    });
+}
